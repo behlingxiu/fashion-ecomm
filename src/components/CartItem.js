05edb87ee2cc203331980a65da889022c7853151
Beh Ling Xiu <behlingxiu@Behs-MacBook-Air.local>
2023-12-19 18:25:19 +0800

    ready to deploy

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import { React, useContext } from "react";
+import { CartContext } from "../contexts/CartContext";
+import { Link } from "react-router-dom";
+import { IoMdClose, IoMdRemove, IoMdAdd } from "react-icons/io";
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+const CartItem = ({ item }) => {
+  const { removeFromCart, increaseAmount, decreaseAmount } =
+    useContext(CartContext);
+  const { id, title, image, price, amount } = item;
+  return (
+    <div className="flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500">
+      <div className="w-full min-h-[150px] flex items-center gap-x-4">
+        {/* image */}
+        <Link to={`/product/${id}`}>
+          <img className="max-w-[80px]" src={image} />
+        </Link>
+        <div className="w-full flex flex-col">
+          {/* title and remove icon */}
+          <div className="flex justify-between mb-2">
+            {/* title */}
+            <Link
+              className="text-sm uppercase font-medium max-w-[240px] text-primary hover:underline"
+              to={`/product/${id}`}
+            >
+              {title}
+            </Link>
+            {/* remove icon */}
+            <div
+              onClick={() => {
+                removeFromCart(id);
+              }}
+              className="text-xl cursor-pointer"
+            >
+              <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
+            </div>
+          </div>
+          <div className=" flex gap-x-2 h-[36px] text-sm">
+            {/* quantity */}
+            <div className="flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium">
+              <div
+                onClick={() => decreaseAmount(id)}
+                className="flex-1 h-full flex justify-center items-center cursor-pointer h-full"
+              >
+                <IoMdRemove />
+              </div>
+              <div className="h-full flex justify-center items-center px-2">
+                {amount}
+              </div>
+              <div
+                onClick={() => {
+                  increaseAmount(id);
+                }}
+                className="flex-1 h-full flex justify-center items-center cursor-pointer"
+              >
+                <IoMdAdd />
+              </div>
+            </div>
+
+            {/* item price */}
+            <div className="flex flex-1 items-center justify-around">
+              RM {price}
+            </div>
+            {/* final price */}
+            <div className="flex flex-1 justify-end items-center text-primary font-medium">
+              RM {`${parseFloat(price * amount).toFixed(2)}`}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
